520138645d4d5b4d9efde64a08429f0fdee62e4d
Oscar Jair Cuadros Nuñez <N00377634@upn.pe>
2023-09-21 23:55:41 -0500

    Add project files.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Por favor ingrese el número límite: ");
+
+int númerolimite = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("******************************");
+Console.WriteLine("******************************");
+Console.WriteLine("Escoja una opción:");
+Console.WriteLine("1) Números pares");
+Console.WriteLine("2) Números impares");
+Console.WriteLine("3) Factorial");
+Console.Write("---> ");
+int opcion = Convert.ToInt32(Console.ReadLine());
+switch (opcion)
+{
+    case 1:
+        for (int i = 1; i <= númerolimite; i++){
+            if (i % 2 == 0){
+                Console.Write(i + ",");
+            }
+        }
+        break;
+    case 2:
+        for (int i = 1; i <= númerolimite; i++){
+            if (i % 2 != 0){
+                Console.Write(i + ",");
+            }
+        }
+        break;
+    case 3:
+        long resultado = 1;
+        for (int i = 1; i <= númerolimite; i++){
+            resultado *= i;
+        }
+        Console.Write("El factorial es " + resultado);
+        break;
+}
